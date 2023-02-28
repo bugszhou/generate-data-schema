b0cd0c5b02d1eba54dadf7eb33723b6ee3ca68c2
youzhao.zhou <youzhao.zhou@verystar.cn>
2023-02-28 15:12:46 +0800

    feat: 数组导出错误

--- src/generateDataSchema.ts
+++ src/generateDataSchema.ts
@@ -30,7 +30,7 @@ function transform(data: any) {
   }
 
   if (dataType === "object" && Array.isArray(data)) {
-    return transformArray(data);
+    return transformArray(data)?.properties ?? Object.create(null);
   }
 
   return transformObject(data);
